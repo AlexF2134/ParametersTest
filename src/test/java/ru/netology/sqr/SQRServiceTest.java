4fad220fa57d02ba28d0e760c0963dd36d530c4b
AlexF2134 <alexeyf1@yandex.ru>
2022-06-16 22:55:35 +0500

    " additional ranges "

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -8,13 +8,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class SQRServiceTest {
 
     @ParameterizedTest
-    @CsvSource(value = {"range 200 - 300, 200, 300, 3"})
-        //Диапазон 200 - 300, итоговое число совпадений 3
-
+    @CsvSource(value = {"range 0 - 150, 0, 150, 3",
+            "range 200 - 300, 200, 300, 3",
+            "range 150 - 500, 150, 500, 10"
+    })
     void shouldMult(String testName, int minValue, int maxValue, int expected) {
         SQRService service = new SQRService();
-        int actual = service.mult(minValue, maxValue);      //Вызов метода
-        assertEquals(expected, actual);          //Выполняется проверка
+        int actual = service.mult(minValue, maxValue);
+        assertEquals(expected, actual);
     }
 }
 
